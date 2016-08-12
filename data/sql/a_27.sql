6b2df9573c96f954a7204c431bcf41401bffa010
Alex Krause <alex.krause@flosports.tv>
2016-08-12 14:48:31 -0400

    Update data sql again

--- data/sql/a_27.sql
+++ data/sql/a_27.sql
@@ -300,7 +300,6 @@ CREATE TABLE IF NOT EXISTS `properties` (
   KEY `status_id` (`status_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
 -- --------------------------------------------------------
 
 --
